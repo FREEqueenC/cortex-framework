f2cd01d97ef739a51bc359092d38cd4d363349bf
Ashleigh Walker <ashleighwalker@anwfoundations.com>
2026-08-31 13:22:04 -0500

    style: apply ruff format cleanups

--- tests/external/common/skills/test_skill_anatomy.py
+++ tests/external/common/skills/test_skill_anatomy.py
@@ -1,4 +1,4 @@
-﻿# Copyright 2026 Google LLC
+# Copyright 2026 Google LLC
 #
 # Licensed under the Apache License, Version 2.0 (the "License");
 # you may not use this file except in compliance with the License.
@@ -13,6 +13,7 @@
 # limitations under the License.
 
 import pathlib
+
 import pytest
 import yaml
 
